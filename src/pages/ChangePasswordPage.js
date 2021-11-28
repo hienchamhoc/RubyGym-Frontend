54e8ac841013ec869931e7f719e4316b80d3a9b4
trandai1106 <tranquangdai11062001@gmail.com>
2021-11-28 09:31:26 +0700

    Add error

---
+++ src/pages/ChangePasswordPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Banner, Footer, Header } from '../components'
+
+function ChangePasswordPage() {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <Footer />
+        </>
+    )
+}
+
+export default ChangePasswordPage
