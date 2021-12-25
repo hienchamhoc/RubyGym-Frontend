c834a389959ba78957f6692108a663db3d167105
dungnq194256 <dunghlhh1@gmail.com>
2021-12-25 15:10:43 +0700

    event page

--- src/routes.js
+++ src/routes.js
@@ -6,22 +6,14 @@ import AdminPage from './pages/AdminPage';
 import TrainerPage from './pages/TrainerPage';
 import CustomerPage from './pages/CustomerPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
-<<<<<<< HEAD
+
 import ServicePage from './pages/ServicePage';
 import EventPage from './pages/EventPage';
 import PackagePage from './pages/PackagePage';
 import BlogPage from './pages/BlogPage';
-=======
-<<<<<<< HEAD
+
 import TestPage from './pages/TestPage';
 
-=======
-import Dich_vu from './pages/Dich_vu';
-import Su_kien from './pages/Su_kien';
-import Goi_tap from './pages/Goi_tap';
-import Blog from './pages/Blog';
->>>>>>> ff89108b49f36071e1f4476d6bd6314041dfead8
->>>>>>> 303bd74cca3df14f54fc400038e398601546703e
 
 const routes = [
     {
